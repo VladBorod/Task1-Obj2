ac11c74e19d99588e3189c42cb277b04bb414dd3
VladBorod <jazgigantsmith@gmail.com>
2022-11-12 20:55:35 +0300

    First task finished

--- Program.cs
+++ Program.cs
@@ -1,3 +1,29 @@
-﻿// Программа, принимающая на вход два числа и выдает какое число большее, а какое меньшее.
+﻿// Задача 1.
+// Программа, принимающая на вход два числа и выдает какое число большее, а какое меньшее.
 
+Console.Clear();
 
+// Ввод сравниваемых чисел.
+
+Console.WriteLine("Numbers compare.");
+
+Console.Write("Enter first number: ");
+int numberA = int.Parse(Console.ReadLine());
+
+Console.Write("Enter second number: ");
+int numberB = int.Parse(Console.ReadLine());
+
+// Сравнение  чисел.
+
+if (numberA > numberB)
+{
+    // YES!
+    Console.WriteLine($"{numberA} > {numberB}");
+}
+else
+{
+    // No!
+    Console.WriteLine($"{numberA} < {numberB}");
+}
+
+// Вывод данных.

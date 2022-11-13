9cbacd961cd236aeb51eae14c4eb1ce8be67873b
VladBorod <jazgigantsmith@gmail.com>
2022-11-13 19:29:05 +0300

    Final final Task1 edition

---
+++ Program1.cs
@@ -0,0 +1,29 @@
+﻿// Задача 1.
+// Программа, принимающая на вход два числа и выдает какое число большее, а какое меньшее.
+
+Console.Clear();
+
+// Ввод сравниваемых чисел.
+
+Console.WriteLine("Numbers compare.");
+
+Console.Write("Enter first number: ");
+int numberA = int.Parse(Console.ReadLine());
+
+Console.Write("Enter second number: ");
+int numberB = int.Parse(Console.ReadLine());
+
+// Сравнение  чисел.
+
+if (numberA > numberB)
+{
+    // YES!
+    Console.WriteLine($"First = {numberA}, Second = {numberB}, max = {numberA}");
+}
+else
+{
+    // No!
+    Console.WriteLine($"First = {numberA}, Second = {numberB}, max = {numberB}");
+}
+
+// Вывод данных.
